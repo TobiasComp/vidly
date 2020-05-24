37d0628b029d05df59c34485b7e93e8bbe8547e4
Tuvia Epstein <tuvia1234@gmail.com>
2020-05-25 00:33:51 +0300

    started the mosh app again

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -15,3 +16,9 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// 3447, 3451, 3453, 3439, 3427
+// 3455, 3366, 3367, 3368, 3369, 3370, 3371, 3373, 3374, 3375, 3376, 3377, 3378, 3379,3380
+// 3381, 3382, 3383, 3384, 3385, 3390, 3391, 3392, 3394, 3396, 3398, 3399
+// 3400, 3401, 3402, 3403, 3408, 3410, 3411, 3413, 3417, 3438, 3439, 3440, 
+// 3441, 3442, 3443, 3444, 3445, 3446, 3447, 34-51, 52-54, 553458, 3459
